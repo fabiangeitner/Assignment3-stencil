04705b38b2af027fb2e79c36c5bab01b77aaca11
milli99 <milli.fiorino@web.de>
2020-05-02 22:54:37 +0200

    created detailseite.html -mf

--- assignment3-stencil/src/components.d.ts
+++ assignment3-stencil/src/components.d.ts
@@ -6,154 +6,164 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-  interface HomeScreen {}
-  interface MyComponent {
-    /**
-     * The first name
-     */
-    first: string;
-    /**
-     * The last name
-     */
-    last: string;
-    /**
-     * The middle name
-     */
-    middle: string;
-  }
-  interface MyDescription {}
-  interface MyHeader {}
-  interface MyPrice {
-    price: string;
-  }
-  interface MySalebanner {}
-  interface ProductVariation {
-    color: string;
-  }
-  interface StartseiteNavigation {}
+    interface HomeScreen {
+    }
+    interface MyComponent {
+        /**
+          * The first name
+         */
+        "first": string;
+        /**
+          * The last name
+         */
+        "last": string;
+        /**
+          * The middle name
+         */
+        "middle": string;
+    }
+    interface MyDescription {
+    }
+    interface MyFilter {
+    }
+    interface MyHeader {
+    }
+    interface MyPrice {
+        "price": string;
+    }
+    interface MySalebanner {
+    }
+    interface ProductVariation {
+        "color": string;
+    }
+    interface StartseiteNavigation {
+    }
 }
 declare global {
-  interface HTMLHomeScreenElement
-    extends Components.HomeScreen,
-      HTMLStencilElement {}
-  var HTMLHomeScreenElement: {
-    prototype: HTMLHomeScreenElement;
-    new (): HTMLHomeScreenElement;
-  };
-  interface HTMLMyComponentElement
-    extends Components.MyComponent,
-      HTMLStencilElement {}
-  var HTMLMyComponentElement: {
-    prototype: HTMLMyComponentElement;
-    new (): HTMLMyComponentElement;
-  };
-  interface HTMLMyDescriptionElement
-    extends Components.MyDescription,
-      HTMLStencilElement {}
-  var HTMLMyDescriptionElement: {
-    prototype: HTMLMyDescriptionElement;
-    new (): HTMLMyDescriptionElement;
-  };
-  interface HTMLMyHeaderElement
-    extends Components.MyHeader,
-      HTMLStencilElement {}
-  var HTMLMyHeaderElement: {
-    prototype: HTMLMyHeaderElement;
-    new (): HTMLMyHeaderElement;
-  };
-  interface HTMLMyPriceElement extends Components.MyPrice, HTMLStencilElement {}
-  var HTMLMyPriceElement: {
-    prototype: HTMLMyPriceElement;
-    new (): HTMLMyPriceElement;
-  };
-  interface HTMLMySalebannerElement
-    extends Components.MySalebanner,
-      HTMLStencilElement {}
-  var HTMLMySalebannerElement: {
-    prototype: HTMLMySalebannerElement;
-    new (): HTMLMySalebannerElement;
-  };
-  interface HTMLProductVariationElement
-    extends Components.ProductVariation,
-      HTMLStencilElement {}
-  var HTMLProductVariationElement: {
-    prototype: HTMLProductVariationElement;
-    new (): HTMLProductVariationElement;
-  };
-  interface HTMLStartseiteNavigationElement
-    extends Components.StartseiteNavigation,
-      HTMLStencilElement {}
-  var HTMLStartseiteNavigationElement: {
-    prototype: HTMLStartseiteNavigationElement;
-    new (): HTMLStartseiteNavigationElement;
-  };
-  interface HTMLElementTagNameMap {
-    "home-screen": HTMLHomeScreenElement;
-    "my-component": HTMLMyComponentElement;
-    "my-description": HTMLMyDescriptionElement;
-    "my-header": HTMLMyHeaderElement;
-    "my-price": HTMLMyPriceElement;
-    "my-salebanner": HTMLMySalebannerElement;
-    "product-variation": HTMLProductVariationElement;
-    "startseite-navigation": HTMLStartseiteNavigationElement;
-  }
+    interface HTMLHomeScreenElement extends Components.HomeScreen, HTMLStencilElement {
+    }
+    var HTMLHomeScreenElement: {
+        prototype: HTMLHomeScreenElement;
+        new (): HTMLHomeScreenElement;
+    };
+    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    }
+    var HTMLMyComponentElement: {
+        prototype: HTMLMyComponentElement;
+        new (): HTMLMyComponentElement;
+    };
+    interface HTMLMyDescriptionElement extends Components.MyDescription, HTMLStencilElement {
+    }
+    var HTMLMyDescriptionElement: {
+        prototype: HTMLMyDescriptionElement;
+        new (): HTMLMyDescriptionElement;
+    };
+    interface HTMLMyFilterElement extends Components.MyFilter, HTMLStencilElement {
+    }
+    var HTMLMyFilterElement: {
+        prototype: HTMLMyFilterElement;
+        new (): HTMLMyFilterElement;
+    };
+    interface HTMLMyHeaderElement extends Components.MyHeader, HTMLStencilElement {
+    }
+    var HTMLMyHeaderElement: {
+        prototype: HTMLMyHeaderElement;
+        new (): HTMLMyHeaderElement;
+    };
+    interface HTMLMyPriceElement extends Components.MyPrice, HTMLStencilElement {
+    }
+    var HTMLMyPriceElement: {
+        prototype: HTMLMyPriceElement;
+        new (): HTMLMyPriceElement;
+    };
+    interface HTMLMySalebannerElement extends Components.MySalebanner, HTMLStencilElement {
+    }
+    var HTMLMySalebannerElement: {
+        prototype: HTMLMySalebannerElement;
+        new (): HTMLMySalebannerElement;
+    };
+    interface HTMLProductVariationElement extends Components.ProductVariation, HTMLStencilElement {
+    }
+    var HTMLProductVariationElement: {
+        prototype: HTMLProductVariationElement;
+        new (): HTMLProductVariationElement;
+    };
+    interface HTMLStartseiteNavigationElement extends Components.StartseiteNavigation, HTMLStencilElement {
+    }
+    var HTMLStartseiteNavigationElement: {
+        prototype: HTMLStartseiteNavigationElement;
+        new (): HTMLStartseiteNavigationElement;
+    };
+    interface HTMLElementTagNameMap {
+        "home-screen": HTMLHomeScreenElement;
+        "my-component": HTMLMyComponentElement;
+        "my-description": HTMLMyDescriptionElement;
+        "my-filter": HTMLMyFilterElement;
+        "my-header": HTMLMyHeaderElement;
+        "my-price": HTMLMyPriceElement;
+        "my-salebanner": HTMLMySalebannerElement;
+        "product-variation": HTMLProductVariationElement;
+        "startseite-navigation": HTMLStartseiteNavigationElement;
+    }
 }
 declare namespace LocalJSX {
-  interface HomeScreen {}
-  interface MyComponent {
-    /**
-     * The first name
-     */
-    first?: string;
-    /**
-     * The last name
-     */
-    last?: string;
-    /**
-     * The middle name
-     */
-    middle?: string;
-  }
-  interface MyDescription {}
-  interface MyHeader {}
-  interface MyPrice {
-    price?: string;
-  }
-  interface MySalebanner {}
-  interface ProductVariation {
-    color?: string;
-  }
-  interface StartseiteNavigation {}
-  interface IntrinsicElements {
-    "home-screen": HomeScreen;
-    "my-component": MyComponent;
-    "my-description": MyDescription;
-    "my-header": MyHeader;
-    "my-price": MyPrice;
-    "my-salebanner": MySalebanner;
-    "product-variation": ProductVariation;
-    "startseite-navigation": StartseiteNavigation;
-  }
+    interface HomeScreen {
+    }
+    interface MyComponent {
+        /**
+          * The first name
+         */
+        "first"?: string;
+        /**
+          * The last name
+         */
+        "last"?: string;
+        /**
+          * The middle name
+         */
+        "middle"?: string;
+    }
+    interface MyDescription {
+    }
+    interface MyFilter {
+    }
+    interface MyHeader {
+    }
+    interface MyPrice {
+        "price"?: string;
+    }
+    interface MySalebanner {
+    }
+    interface ProductVariation {
+        "color"?: string;
+    }
+    interface StartseiteNavigation {
+    }
+    interface IntrinsicElements {
+        "home-screen": HomeScreen;
+        "my-component": MyComponent;
+        "my-description": MyDescription;
+        "my-filter": MyFilter;
+        "my-header": MyHeader;
+        "my-price": MyPrice;
+        "my-salebanner": MySalebanner;
+        "product-variation": ProductVariation;
+        "startseite-navigation": StartseiteNavigation;
+    }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
-  export namespace JSX {
-    interface IntrinsicElements {
-      "home-screen": LocalJSX.HomeScreen &
-        JSXBase.HTMLAttributes<HTMLHomeScreenElement>;
-      "my-component": LocalJSX.MyComponent &
-        JSXBase.HTMLAttributes<HTMLMyComponentElement>;
-      "my-description": LocalJSX.MyDescription &
-        JSXBase.HTMLAttributes<HTMLMyDescriptionElement>;
-      "my-header": LocalJSX.MyHeader &
-        JSXBase.HTMLAttributes<HTMLMyHeaderElement>;
-      "my-price": LocalJSX.MyPrice & JSXBase.HTMLAttributes<HTMLMyPriceElement>;
-      "my-salebanner": LocalJSX.MySalebanner &
-        JSXBase.HTMLAttributes<HTMLMySalebannerElement>;
-      "product-variation": LocalJSX.ProductVariation &
-        JSXBase.HTMLAttributes<HTMLProductVariationElement>;
-      "startseite-navigation": LocalJSX.StartseiteNavigation &
-        JSXBase.HTMLAttributes<HTMLStartseiteNavigationElement>;
+    export namespace JSX {
+        interface IntrinsicElements {
+            "home-screen": LocalJSX.HomeScreen & JSXBase.HTMLAttributes<HTMLHomeScreenElement>;
+            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "my-description": LocalJSX.MyDescription & JSXBase.HTMLAttributes<HTMLMyDescriptionElement>;
+            "my-filter": LocalJSX.MyFilter & JSXBase.HTMLAttributes<HTMLMyFilterElement>;
+            "my-header": LocalJSX.MyHeader & JSXBase.HTMLAttributes<HTMLMyHeaderElement>;
+            "my-price": LocalJSX.MyPrice & JSXBase.HTMLAttributes<HTMLMyPriceElement>;
+            "my-salebanner": LocalJSX.MySalebanner & JSXBase.HTMLAttributes<HTMLMySalebannerElement>;
+            "product-variation": LocalJSX.ProductVariation & JSXBase.HTMLAttributes<HTMLProductVariationElement>;
+            "startseite-navigation": LocalJSX.StartseiteNavigation & JSXBase.HTMLAttributes<HTMLStartseiteNavigationElement>;
+        }
     }
-  }
 }
--- assignment3-stencil/src/components/startseite-navigation/startseite-navigation.tsx
+++ assignment3-stencil/src/components/startseite-navigation/startseite-navigation.tsx
@@ -1,41 +1,29 @@
-import { Component, ComponentInterface, Host, h, } from '@stencil/core';
+import { Component, ComponentInterface, Host, h } from "@stencil/core";
 
 @Component({
-  tag: 'startseite-navigation',
-  styleUrl: 'startseite-navigation.css',
+  tag: "startseite-navigation",
+  styleUrl: "startseite-navigation.css",
   shadow: true,
 })
 export class StartseiteNavigation implements ComponentInterface {
-
-
   private handleHome(e: any) {
-    return (
-      e.currentTarget.classList.toggle("changeHome")
-    );
+    return e.currentTarget.classList.toggle("changeHome");
   }
 
   private handleCloth(e: any) {
-    return (
-      e.currentTarget.classList.toggle("changeCloth")
-    );
+    return e.currentTarget.classList.toggle("changeCloth");
   }
 
   private handleHeart(e: any) {
-    return (
-      e.currentTarget.classList.toggle("changeHeart")
-    );
+    return e.currentTarget.classList.toggle("changeHeart");
   }
 
   private handleBag(e: any) {
-    return (
-      e.currentTarget.classList.toggle("changeBag")
-    );
+    return e.currentTarget.classList.toggle("changeBag");
   }
 
   private handleUser(e: any) {
-    return (
-      e.currentTarget.classList.toggle("changeUser")
-    );
+    return e.currentTarget.classList.toggle("changeUser");
   }
 
   render() {
@@ -43,16 +31,34 @@ export class StartseiteNavigation implements ComponentInterface {
       <Host>
         <div class="nav">
           <ul>
-            <li onClick={(e) => this.handleHome(e)}><a href="#"><img src="../Bilder/home-run.png" alt="Home" /></a></li>
-            <li onClick={(e) => this.handleCloth(e)}><a href="#"><img src="../Bilder/cloth.png" alt="T-shirt" /></a></li>
-            <li onClick={(e) => this.handleHeart(e)}><a href="#"><img src="../Bilder/heart.png" alt="Favoriten" /></a></li>
-            <li onClick={(e) => this.handleBag(e)}><a href="#"><img src="../Bilder/bag.png" alt="Einkaufstasche" /></a></li>
-            <li onClick={(e) => this.handleUser(e)}><a href="#"><img src="../Bilder/user.png" alt="Konto" /></a></li>
+            <li onClick={(e) => this.handleHome(e)}>
+              <a href="index.html">
+                <img src="../Bilder/home-run.png" alt="Home" />
+              </a>
+            </li>
+            <li onClick={(e) => this.handleCloth(e)}>
+              <a href="detailseite.html">
+                <img src="../Bilder/cloth.png" alt="T-shirt" />
+              </a>
+            </li>
+            <li onClick={(e) => this.handleHeart(e)}>
+              <a href="#">
+                <img src="../Bilder/heart.png" alt="Favoriten" />
+              </a>
+            </li>
+            <li onClick={(e) => this.handleBag(e)}>
+              <a href="#">
+                <img src="../Bilder/bag.png" alt="Einkaufstasche" />
+              </a>
+            </li>
+            <li onClick={(e) => this.handleUser(e)}>
+              <a href="#">
+                <img src="../Bilder/user.png" alt="Konto" />
+              </a>
+            </li>
           </ul>
         </div>
-
       </Host>
     );
   }
-
 }
