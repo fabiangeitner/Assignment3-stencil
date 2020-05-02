3c9b5c6a9ba00c445b2a9d0ea9edf268a2664be3
milli99 <milli.fiorino@web.de>
2020-05-02 14:13:27 +0200

    added onclick on img

--- assignment3-stencil/src/components/my-header/my-header.tsx
+++ assignment3-stencil/src/components/my-header/my-header.tsx
@@ -6,7 +6,13 @@ import { Component, ComponentInterface, Host, h } from "@stencil/core";
   shadow: true,
 })
 export class MyHeader implements ComponentInterface {
+  showInput() {
+    let inputfield = `<input id="searchInput" type="text" />`;
+    console.log(inputfield);
+  }
+
   render() {
+    let inputfield = <input id="searchInput" type="text" />;
     return (
       <Host>
         <div class="wrapper">
@@ -14,7 +20,14 @@ export class MyHeader implements ComponentInterface {
             <p>A R N É E</p>
           </div>
           <div class="search">
-            <img src="../Bilder/search.png" alt="Search" />
+            {inputfield}
+            <img
+              src="../Bilder/search.png"
+              alt="Search"
+              onClick={() => {
+                this.showInput();
+              }}
+            />
           </div>
         </div>
       </Host>
