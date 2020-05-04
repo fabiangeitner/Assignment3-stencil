d97d8dce213ebba461a866e2f962b85ba3513704
milli99 <milli.fiorino@web.de>
2020-05-04 22:21:17 +0200

    added linking on enter

--- assignment3-stencil/src/components/my-header/my-header.tsx
+++ assignment3-stencil/src/components/my-header/my-header.tsx
@@ -6,22 +6,21 @@ import { Component, ComponentInterface, Host, h, State } from "@stencil/core";
   shadow: true,
 })
 export class MyHeader implements ComponentInterface {
-  @State() inputSearch!: HTMLInputElement;
+  @State() inputSearch: HTMLInputElement;
 
   showhideInput() {
-    this.inputSearch.addEventListener("keyup", function (event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        //console.log(this.inputSearch.value);
-
-        console.log("suchen");
-      }
-    });
     if (this.inputSearch.style.display == "none") {
       this.inputSearch.style.display = "block";
     } else {
       this.inputSearch.style.display = "none";
     }
+    this.inputSearch.addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+
+        window.location.href = "productlist.html";
+      }
+    });
   }
 
   render() {
@@ -35,7 +34,9 @@ export class MyHeader implements ComponentInterface {
             <img
               src="../Bilder/search.png"
               alt="Search"
-              onClick={() => this.showhideInput()}
+              onClick={() => {
+                this.showhideInput();
+              }}
             />
             <input
               id="searchInput"
